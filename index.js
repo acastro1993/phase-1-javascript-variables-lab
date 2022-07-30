6759f7261977ab6f52323c26147710a94b02b653
Alberto Castro <Alberto1973@live.com>
2022-07-30 16:33:31 -0400

    commit tree b9bc3fc3524ecf72f510a6f467111b6231e57a61

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+
+mostProfitableNeighborhood = 'Chelsea'
+companyCeo = 'Susan Smith'
+const companyName = 'Scuber';
+    it('companyName', function () {
+        expect('companyName').to.equal('Chelsea');
+    });
+let mostProfitableNeighborhood = 'Chelsea';
+    it('mostProfitableNeighborhood', function () {
+        expect(mostProfitableNeighborhood).to.equal('Chelsea');
+    });
+let companyCeo = 'Susan Smith'
+    it('companyCeo', function () {
+        expect(companyCeo).to.equal('Susan Smith');
+    });
